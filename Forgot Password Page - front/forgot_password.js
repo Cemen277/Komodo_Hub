251a9757edf83ff6d5181e3ac445241fdc33ef94
johnson3r33 <johnson.reji03@gmail.com>
2025-03-25 23:20:00 +0000

    Updated forgot_password.js

--- Forgot Password Page - front/forgot_password.js
+++ Forgot Password Page - front/forgot_password.js
@@ -1,9 +1,9 @@
 document.addEventListener("touchstart", function() {}, true);
 
 document.getElementById("forgot_password_form").addEventListener("submit", function(event) {
-    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    if (email == ""){
+    if (password == ""){
         alert("Please fill in the field");
         event.preventDefault();
     }
