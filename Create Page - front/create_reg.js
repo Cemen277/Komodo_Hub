6d65b2c5d48227e6192c19635302f0f9dc720cd0
johnson3r33 <johnson.reji03@gmail.com>
2025-03-27 19:55:16 +0000

    Updated create_reg.js

--- Create Page - front/create_reg.js
+++ Create Page - front/create_reg.js
@@ -1,12 +1,25 @@
 document.addEventListener("touchstart", function() {}, true);
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
     const textarea = document.getElementById("user_message");
-    const wordCounter = document.getElementById("letter_counter");
+
+    textarea.addEventListener("input", function(){ 
+        this.style.height = "auto";
+        this.style.height = (this.scrollHeight) + "px";
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() { // For Letter counter
+    const textarea = document.getElementById("user_message");
+    const letterCounter = document.getElementById("letter_counter");
     const maxLength = textarea.getAttribute("maxlength");
 
     textarea.addEventListener("input", function() {
         const letterCount = textarea.value.trim().length;
-        wordCounter.textContent = `${letterCount}/${maxLength}`;
+        letterCounter.textContent = `${letterCount}/${maxLength}`;
     });
+});
+
+document.getElementById("upload_container").addEventListener("click", function(){
+    document.getElementById("file_upload").click();
 });
