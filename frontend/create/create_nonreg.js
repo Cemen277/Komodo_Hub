4539afdf5e7b81724540e3c74d0307a17a03f351
Cemen Boianivskyi <cemenboianivskyi@gmail.com>
2025-04-02 02:37:17 +0100

    Organising directory

---
+++ frontend/create/create_nonreg.js
@@ -0,0 +1,41 @@
+document.addEventListener("touchstart", function() {}, true);
+document.addEventListener("DOMContentLoaded", function() {
+    const user_type = localStorage.getItem('user_type');
+    const home_page = document.getElementById("home_page");
+    const account_page = document.getElementById("account_page");
+    const community_page = document.getElementById("community_page");
+    const library_page = document.getElementById("library_page");
+    const create_page = document.getElementById("create_page");
+    home_page.addEventListener("click", function(){
+        if (user_type == "guest"){
+            window.location.href = "../Home Page - front/home_page_nonreg.html";
+        }else {
+            window.location.href = "../Home Page - front/home_page.html";
+        }
+    })
+    create_page.addEventListener("click", function(){
+        if (user_type == "guest"){
+            window.location.href = "../Create Page - front/create_nonreg.html";
+        }else {
+            window.location.href = "../Create Page - front/create_reg.htm";
+        }
+    })
+    library_page.addEventListener("click", function(){
+        if (user_type == "guest"){
+            window.location.href = "../Library_Page/library.html";
+        }
+    })
+    account_page.addEventListener("click", function(){
+        if (user_type == "guest"){
+            window.location.href = "../Account  Page - front/nonreg.html";
+        }
+        else{
+            window.location.href = "../Account  Page - front/registered.html";
+        }
+    })
+    community_page.addEventListener("click", function(){
+        if (user_type == "guest"){
+            window.location.href = "../Community Page - front/community_hub.html";
+        }
+    })
+});
